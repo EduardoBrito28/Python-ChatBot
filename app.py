0c8e3c2fae98e4e86848bc8fe612dc10cdf775df
Eduardo Brito <eduardo370@outlook.pt>
2025-01-30 18:21:59 +0100

    permitir armazenar os ficheiros localmente com identificadores na BD

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from utils import chatbot, text
 from streamlit_chat import message
+import os
+import hashlib
 
 
 def main():
@@ -27,13 +29,32 @@ def main():
 
         if st.button("Processar"):
             for pdf in pdf_docs:
-                file_text = text.process_files([pdf])
-                text.save_document_metadata(pdf.name, file_text)
-                chunks = text.create_text_chunks(file_text)
-                vectorstore = chatbot.create_vectore(chunks)
-                st.session_state.conversation = chatbot.create_conversation_chain(
-                    vectorstore
-                )
+                # Calcular o hash do arquivo
+                pdf_hash = hashlib.md5(pdf.getvalue()).hexdigest()
+
+                # Verificar se o hash já existe no banco de dados
+                if text.document_exists(pdf_hash):
+                    st.warning(
+                        f"O arquivo '{pdf.name}' já foi carregado anteriormente."
+                    )
+                else:
+                    # Definir o caminho completo para salvar o arquivo na subpasta
+                    subpasta = os.path.join(os.getcwd(), "arquivos")
+                    if not os.path.exists(subpasta):
+                        os.makedirs(subpasta)
+                    caminho_arquivo = os.path.join(subpasta, pdf.name)
+
+                    # Salvar o arquivo PDF na subpasta 'arquivos'
+                    with open(caminho_arquivo, "wb") as f:
+                        f.write(pdf.getbuffer())
+
+                    file_text = text.process_files([caminho_arquivo])
+                    text.save_document_metadata(pdf.name, file_text, pdf_hash)
+                    chunks = text.create_text_chunks(file_text)
+                    vectorstore = chatbot.create_vectore(chunks)
+                    st.session_state.conversation = chatbot.create_conversation_chain(
+                        vectorstore
+                    )
 
         if st.button("Ver Histórico"):
             history = chatbot.get_chat_history()
--- utils/text.py
+++ utils/text.py
@@ -2,13 +2,15 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from utils import db
 
+
 def process_files(files):
     text = ""
     for file in files:
         pdf = PdfReader(file)
         for page in pdf.pages:
             text += page.extract_text()
-        return text
+    return text
+
 
 def create_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -17,10 +19,15 @@ def create_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-def save_document_metadata(filename, content):
+
+def save_document_metadata(filename, content, file_hash):
     document_data = {
         "filename": filename,
         "content": content,
+        "hash": file_hash,
     }
-    pass
     db.documents_collection.insert_one(document_data)
+
+
+def document_exists(file_hash):
+    return db.documents_collection.find_one({"hash": file_hash}) is not None
