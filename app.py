857b762f06db969dc2870589fcff7237a18b8179
Eduardo Brito <eduardo370@outlook.pt>
2025-01-31 16:33:28 +0100

    Initial

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from utils import chatbot, text
 from streamlit_chat import message
+import os
+import hashlib
+
 
 def main():
     st.set_page_config(page_title="UBV Chatbot", page_icon=":books:")
@@ -11,16 +14,12 @@ def main():
         st.session_state.conversation = None
 
     if user_question:
-        # Recuperar documentos relevantes do MongoDB
-        relevant_documents = chatbot.retrieve_relevant_documents(user_question)
-        # Gerar resposta com base nos documentos relevantes
-        response = chatbot.generate_response(user_question, relevant_documents)
+        response = st.session_state.conversation(user_question)["chat_history"]
+        for i, text_message in enumerate(response):
+            message(text_message.content, is_user=(i % 2 == 0), key=str(i) + "_user")
+        # Salvar a pergunta e a resposta no MongoDB
         if response:
-            message(response, is_user=False)
-            # Salvar a pergunta e a resposta no MongoDB
-            chatbot.save_chat(user_question, response)
-        else:
-            st.warning("Nenhuma resposta foi gerada para a sua pergunta.")
+            chatbot.save_chat(user_question, response[-1].content)
 
     with st.sidebar:
         st.subheader("Seus arquivos")
@@ -30,24 +29,39 @@ def main():
 
         if st.button("Processar"):
             for pdf in pdf_docs:
-                file_hash = text.generate_file_hash(pdf)
-                if text.document_exists(file_hash):
-                    st.warning(f"O arquivo {pdf.name} já existe no banco de dados.")
-                    continue
+                # Calcular o hash do arquivo
+                pdf_hash = hashlib.md5(pdf.getvalue()).hexdigest()
 
-                file_text = text.process_files([pdf])
-                text.save_document_metadata(pdf.name, file_text, file_hash)
-                chunks = text.create_text_chunks(file_text)
-                vectorstore = chatbot.create_vectorstore_from_documents(chunks)
-                st.session_state.conversation = chatbot.create_conversation_chain(
-                    vectorstore
-                )
+                # Verificar se o hash já existe no banco de dados
+                if text.document_exists(pdf_hash):
+                    st.warning(
+                        f"O arquivo '{pdf.name}' já foi carregado anteriormente."
+                    )
+                else:
+                    # Definir o caminho completo para salvar o arquivo na subpasta
+                    subpasta = os.path.join(os.getcwd(), "arquivos")
+                    if not os.path.exists(subpasta):
+                        os.makedirs(subpasta)
+                    caminho_arquivo = os.path.join(subpasta, pdf.name)
+
+                    # Salvar o arquivo PDF na subpasta 'arquivos'
+                    with open(caminho_arquivo, "wb") as f:
+                        f.write(pdf.getbuffer())
+
+                    file_text = text.process_files([caminho_arquivo])
+                    text.save_document_metadata(pdf.name, file_text, pdf_hash)
+                    chunks = text.create_text_chunks(file_text)
+                    vectorstore = chatbot.create_vectore(chunks)
+                    st.session_state.conversation = chatbot.create_conversation_chain(
+                        vectorstore
+                    )
 
         if st.button("Ver Histórico"):
             history = chatbot.get_chat_history()
             for chat in history:
                 st.write(f"**Pergunta:** {chat['question']}")
                 st.write(f"**Resposta:** {chat['answer']}")
+
 
 if __name__ == "__main__":
     main()
